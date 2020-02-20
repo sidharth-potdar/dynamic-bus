c8295467f4eea55d9666c6d0d9fbf612072cc349
Harsha Muddu <hmuddu3@gatech.edu>
2020-02-20 18:01:36 -0500

    Schema updated for if not exists

--- db_logging/event_logger.py
+++ db_logging/event_logger.py
@@ -1,5 +1,8 @@
 class EventLogger():
 
-	def __init__(self):
-		pass
+	def __init__(self, db_name='simlog.db'):
+		self.conn = sqlite3.connect(db_name)
 		
+		with open(r'logging_schema.txt','r') as log_schema_file:
+			table_creation = []
+			for
